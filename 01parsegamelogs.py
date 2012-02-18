f6b33c11529f1917606b93a53f5cffeceda62db8
Matt Spitz <mattspitz@gmail.com>
2012-02-18 14:59:04 -0500

    Parsing game states into feature dicts.  Includes retrosheet_id so we can augment the dicts later with more info on players.

--- 01parsegamelogs.py
+++ 01parsegamelogs.py
@@ -3,70 +3,93 @@
 # input: filename(s) in EVA/EVN format
 # output: training set representing labelled situations
 
-from common import FeatureSet
+from common import FeatureSet, GameState, PlayerState, Label
 import json
+import logging
 import re
 import sys
-
-class GameState:
-    __slots__ = [
-        "visteam",
-        "hometeam"
-        ]
-
-    def __str__(self):
-        return "<GameState: %s>" % ", ".join([ "%s=%s" % (slot, getattr(self, slot, None)) for slot in self.__slots__ ])
-
-class PlayerState:
-    __slots__ = [
-        "retrosheet_id",
-        "team",
-        "name",
-        "field_pos",
-        "bat_pos"
-        ]
-
-    def __str__(self):
-        return "<PlayerState: %s>" % ", ".join([ "%s=%s" % (slot, getattr(self, slot, None)) for slot in self.__slots__ ])
-
-    def __repr__(self):
-        return self.__str__()
 
 def parse_header(header):
     game_state = GameState()
     base_featureset = FeatureSet()
 
     for line in header.split("\n"):
+        line = line.strip()
         if line.startswith("info,"):
-            _, key, value = line.strip().split(",")
+            _, key, value = line.split(",")
 
             if key in ["visteam", "hometeam"]:
                 setattr(GameState, key, value)
 
-            if key in FeatureSet.__slots__:
-                setattr(FeatureSet, key, value)
+            fs_key = "game_%s" % key
+            if fs_key in FeatureSet.__slots__:
+                setattr(FeatureSet, fs_key, value)
 
     return game_state, base_featureset
 
-def parse_players(players):
+def add_player_state(line, game_state, state_by_id):
+    # note, this doesn't keep track of who gets subbed out, just the latest state for each player
+    _, retrosheetid, name, team, batpos, fieldpos = line.split(",")
+
+    player_state = PlayerState()
+    player_state.retrosheetid = retrosheetid
+    player_state.name = name
+    player_state.visorhome = "visteam" if team == "0" else "hometeam"
+    player_state.team = getattr(game_state, player_state.visorhome)
+    player_state.batpos = batpos
+    player_state.fieldpos = fieldpos
+
+    state_by_id[retrosheetid] = player_state
+
+def parse_players(players, game_state):
     state_by_id = {}
 
     for line in players.split("\n"):
+        line = line.strip()
         if line.startswith("start,"):
-            _, retrosheet_id, name, team, bat_pos, field_pos = line.strip().split(",")
-            if retrosheet_id in state_by_id:
-                raise Exception("Tried to add %s, which had been previously recorded.  Old: %s, new: %s" % (retrosheet_id, state_by_id[retrosheet_id], line.strip()))
-
-            player_state = PlayerState()
-            player_state.retrosheet_id = retrosheet_id
-            player_state.name = name
-            player_state.team = "visteam" if team == "1" else "hometeam"
-            player_state.bat_pos = bat_pos
-            player_state.field_pos = field_pos
-
-            state_by_id[retrosheet_id] = player_state
+            add_player_state(line, game_state, state_by_id)
 
     return state_by_id
+
+def get_feature_sets(playbyplay, game_state, base_featureset, player_state_by_id):
+    for line in playbyplay.split("\n"):
+        line = line.strip()
+
+        if line.startswith("sub"):
+            add_player_state(line, game_state, player_state_by_id)
+
+        elif line.startswith("play"):
+            _, inning, visorhome, retrosheetid, count, pitches, play = line.split(",")
+
+            if any( play.startswith(ignore) for ignore in ["NP"] ):
+                continue
+
+            try:
+                featureset = base_featureset.copy()
+
+                # player
+                featureset.add_player_info(player_state_by_id[retrosheetid])
+
+                # at-bat stats
+                featureset.ab_inning = inning
+                numballs, numstrikes = count
+                featureset.ab_numballs = numballs
+                featureset.ab_numstrikes = numstrikes
+
+                # TODO: keep track of outs, runners on?
+
+                # label
+                if play.startswith("HR"):
+                    featureset.label = Label.HR
+                elif play.startswith("K"):
+                    featureset.label = Label.K
+                else:
+                    featureset.label = Label.OTHER
+
+                yield featureset
+            except Exception:
+                logging.exception("Choked on line: %s" % line)
+                raise
 
 def get_game_sections(game):
     sections_regex = re.compile(r"(?P<header>^id,.*?)"
@@ -80,9 +103,9 @@ def parse_game(game):
     sections = get_game_sections(game)
 
     game_state, base_featureset = parse_header(sections.group("header"))
-    player_state_by_id = parse_players(sections.group("players"))
+    player_state_by_id = parse_players(sections.group("players"), game_state)
 
-    for feature_set in get_feature_sets(base_featureset, player_state_by_id):
+    for feature_set in get_feature_sets(sections.group("playbyplay"), game_state, base_featureset, player_state_by_id):
         print feature_set
 
 def parse_ev(ev_fn):
--- common.py
+++ common.py
@@ -1,14 +1,59 @@
 import json
 
+class Label:
+    HR = "HR"
+    K = "K"
+    OTHER = "OTHER"
+
+class GameState:
+    __slots__ = [
+        "visteam",
+        "hometeam"
+        ]
+
+    def __str__(self):
+        return "<GameState: %s>" % ", ".join([ "%s=%s" % (slot, getattr(self, slot, None)) for slot in self.__slots__ ])
+
+class PlayerState:
+    __slots__ = [
+        "retrosheetid",
+        "visorhome",
+        "team",
+        "name",
+        "fieldpos",
+        "batpos"
+        ]
+
+    def __str__(self):
+        return "<PlayerState: %s>" % ", ".join([ "%s=%s" % (slot, getattr(self, slot, None)) for slot in self.__slots__ ])
+
+    def __repr__(self):
+        return self.__str__()
+
 class FeatureSet:
     __slots__ = [
         # general, per-game
-        "daynight",
-        "date",
-        "number", # 0 is the first game of the day, 1 is the second of a doubleheader
-        "site", # ballpark
-        "temp",
-        "winddir"
+        "game_daynight",
+        "game_date",
+        "game_number", # 0 is the first game of the day, 1 is the second of a doubleheader
+        "game_site", # ballpark
+        "game_temp",
+        "game_winddir",
+
+        # at-bat
+        "ab_inning",
+        "ab_numballs",
+        "ab_numstrikes",
+
+        # player stats
+        "player_fieldpos",
+        "player_batpos",
+        "player_team",
+        "player_visorhome",
+        "player_retrosheetid",
+
+        # label
+        "label"
         ]
 
     def __str__(self):
@@ -21,5 +66,10 @@ class FeatureSet:
     def copy(self):
         obj = FeatureSet()
         for s in self.__slots__:
-            setattr(obj, s, getattr(self, s))
+            if hasattr(self, s):
+                setattr(obj, s, getattr(self, s))
         return obj
+
+    def add_player_info(self, player_state):
+        for field in ["fieldpos", "batpos", "team", "visorhome", "retrosheetid"]:
+            setattr(self, "player_%s" % field, getattr(player_state, field))
