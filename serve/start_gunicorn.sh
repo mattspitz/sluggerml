1511394d659dc957dc7ebfe2a5f74e1816ca253a
Matt Spitz <mspitz@ebay.com>
2012-02-26 16:46:41 -0500

    Don't complicate gunicorny things.

--- serve/start_gunicorn.sh
+++ serve/start_gunicorn.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-gunicorn wsgi:application -b unix:/tmp/gunicorn.sock -k gevent -w 4
+#gunicorn wsgi:application -b unix:/tmp/gunicorn.sock -k gevent -w 4
+
+gunicorn wsgi:application -b unix:/tmp/gunicorn.sock
